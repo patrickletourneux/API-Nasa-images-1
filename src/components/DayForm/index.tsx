b312bdb33e8aa6a26d0ce8784bfacfb60c92486b
patrick letourneux <patrick.letourneux20@gmail.com>
2022-10-06 14:09:27 +0200

    DayForm class Component

--- src/components/DayForm/index.tsx
+++ src/components/DayForm/index.tsx
@@ -1,21 +1,29 @@
-import React from 'react';
+
+import React, { Component } from 'react'
 import './index.css';
 
 type Props = {
-    onChange:React.ChangeEventHandler<HTMLInputElement>
+  onChange:React.ChangeEventHandler<HTMLInputElement>
 }
 
-const DayForm = (props: Props) => {
-  return (
-        <form className='day-form'>
-          <input 
-            className='date-input'
-            type='date'
-            onChange = {props.onChange}
-            >
-          </input >
-        </form>
-  )
+type State = {}
+
+class DayForm extends Component<Props, State> {
+  state = {}
+
+  render() {
+    return (
+      <form className='day-form'>
+        <input 
+          className='date-input'
+          type='date'
+          onChange = {this.props.onChange}
+          >
+        </input >
+      </form>
+    )
+  }
 }
 
 export default DayForm
+
