bc656826f999eb836550ec701e70f5b98f61e435
patrick letourneux <patrick.letourneux20@gmail.com>
2022-10-06 14:02:08 +0200

    CardVideo in class

--- src/components/CardVideo/index.tsx
+++ src/components/CardVideo/index.tsx
@@ -1,26 +1,32 @@
+
+import React, { Component } from 'react'
 import './index.css';
 
 import type { DayData } from '../../typescript/type';
 
 type Props = {
-    dayData: DayData
+  dayData: DayData
 }
 
-const CardVideo = (props: Props) => {
+type State = {}
 
-  // const filename = props.src.replace(/^.*[\\/]/, '')
+class CardVideo extends Component<Props, State> {
+  state = {}
 
-  return (
-    <div className="CardVideo">
-      <iframe 
-        title={props.dayData.title}
-        width="300" 
-        height="250"
-        allowFullScreen={true}
-        src={props.dayData.url}>
-      </iframe>
-    </div>
-  )
+  render() {
+    return (
+      <div className="CardVideo">
+        <iframe 
+          title={this.props.dayData.title}
+          width="300" 
+          height="250"
+          allowFullScreen={true}
+          src={this.props.dayData.url}>
+        </iframe>
+      </div>
+    )
+  }
 }
 
-export default CardVideo;
+export default CardVideo
+
